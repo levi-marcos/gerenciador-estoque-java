7659d357dbe7f72d45ef4bd8c4cf288afac84ee1
Levi <marcos.levi.contato@gmail.com>
2026-05-08 20:54:44 -0300

    feat: exibicao de estoque e a soma total dos produtos

--- src/Main.java
+++ src/Main.java
@@ -19,12 +19,28 @@ public class Main {
 
                 System.out.println("VALOR DO PRODUTO: ");
                 double valorProduto = leitor.nextDouble();
+                leitor.nextLine();
+
+                estoque.put(nomeProduto, valorProduto);
+
             } else if (opcaoAdicionar.equalsIgnoreCase("N")){
                 break;
             } else {
                 System.out.println("OPÇÃO INVÁLIDA, DIGITE NOVAMENTE!");
             }
         }
+
+        double somaTotalProdutos = 0;
+        System.out.println("\nRelatório dos produtos do Estoque!");
+
+
+        for (String nomeProduto: estoque.keySet()){
+            double precoProduto = estoque.get(nomeProduto);
+            System.out.println("Produto: " + nomeProduto + " | Preço: R$ " + precoProduto);
+            somaTotalProdutos += precoProduto;
+        }
+
+        System.out.println("VALOR TOTAL DOS PRODUTOS É DE: " + somaTotalProdutos);
 
     }
 }
